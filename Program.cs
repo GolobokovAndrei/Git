c41206873cda78c77aa1170041fa4488a8945739
fasterplaymc <agolobokov1@gmail.com>
2024-11-14 18:21:26 +0700

    Change text

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Some text!");
             Console.ReadLine();
         }
     }
